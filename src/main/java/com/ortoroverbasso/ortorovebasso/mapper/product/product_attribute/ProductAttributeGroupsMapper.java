f168b9f5c69ba51c2b29fb52403fa1627ddf07a1
PecchiniWellington <wellingtonpecchini@gmail.com>
2025-04-21 15:33:18 +0200

    add product atributes bean

--- src/main/java/com/ortoroverbasso/ortorovebasso/mapper/product/product_attribute/ProductAttributeGroupsMapper.java
+++ src/main/java/com/ortoroverbasso/ortorovebasso/mapper/product/product_attribute/ProductAttributeGroupsMapper.java
@@ -1,8 +1,11 @@
 package com.ortoroverbasso.ortorovebasso.mapper.product.product_attribute;
 
+import org.springframework.stereotype.Component;
+
 import com.ortoroverbasso.ortorovebasso.dto.product.product_attributes.ProductAttributeGroupDto;
 import com.ortoroverbasso.ortorovebasso.entity.product.product_attributes.ProductAttributeGroupsEntity;
 
+@Component
 public class ProductAttributeGroupsMapper {
 
     // Metodo di istanza, non statico
